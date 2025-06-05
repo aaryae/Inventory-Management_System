f7dd64713acadccd5aa9c08ca7c2e7ecd36da7f7
aaryae <aaryaed@gmail.com>
2025-06-05 14:07:42 +0545

    feat: security config added

--- src/main/java/com/example/inventorymanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/example/inventorymanagementsystem/config/SecurityConfig.java
@@ -1,6 +1,31 @@
 package com.example.inventorymanagementsystem.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig  {
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+@Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+
+    http.authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/register").permitAll()
+                    .requestMatchers("/api/login").permitAll()
+                    .anyRequest().authenticated())
+            .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .csrf(csrf -> csrf.disable());
+    return http.build();
+}
 
 
 }

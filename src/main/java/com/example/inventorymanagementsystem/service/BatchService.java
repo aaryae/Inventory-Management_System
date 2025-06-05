05c40245ebf5f90a9e5a001477c727e54f17855e
Sugam Shakya <mrsugamshakya@gmail.com>
2025-06-05 14:19:30 +0545

    feat: add service interface for resource, batch and master data

--- src/main/java/com/example/inventorymanagementsystem/service/BatchService.java
+++ src/main/java/com/example/inventorymanagementsystem/service/BatchService.java
@@ -1,4 +1,18 @@
 package com.example.inventorymanagementsystem.service;
 
+import com.example.inventorymanagementsystem.dtos.request.resource.ResourceRequestDTO;
+import com.example.inventorymanagementsystem.dtos.response.resource.ResourceResponseDTO;
+
+import java.util.List;
+
 public interface BatchService {
+
+    ResourceResponseDTO createBatch(ResourceRequestDTO batchRequestDTO);
+
+    ResourceResponseDTO getBatchById (Long batch_id);
+
+    List<ResourceResponseDTO> getAllBatches();
+
+    List<ResourceResponseDTO> getResourcesByBatchId(Long batch_id);
+
 }
--- src/main/java/com/example/inventorymanagementsystem/service/MasterDataService.java
+++ src/main/java/com/example/inventorymanagementsystem/service/MasterDataService.java
@@ -1,4 +1,26 @@
 package com.example.inventorymanagementsystem.service;
 
+import com.example.inventorymanagementsystem.model.ResourceClass;
+import com.example.inventorymanagementsystem.model.ResourceStatus;
+import com.example.inventorymanagementsystem.model.ResourceType;
+
+import java.util.List;
+
 public interface MasterDataService {
+
+    // This is used to fetch all the master data
+    List<ResourceType> getAllResourceTypes();
+
+    List<ResourceClass> getAllResourceClass();
+
+    List<ResourceStatus> getAllResourceStatus();
+
+
+
+    // This is used for validation and lookup by an ID
+    ResourceType getResourceTypeById(Long resource_id);
+
+    ResourceClass getResourceClassById(Long resource_id);
+
+    ResourceStatus getResourceStatusById(Long resource_id);
 }

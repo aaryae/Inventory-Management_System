0a7ca97b3cf1564a050238da9cf603b46130b659
Sugam Shakya <mrsugamshakya@gmail.com>
2025-06-09 11:09:37 +0545

    feat: implement getAllResources method with response DTO mapping

--- src/main/java/com/example/inventorymanagementsystem/service/impl/ResourceServiceImpl.java
+++ src/main/java/com/example/inventorymanagementsystem/service/impl/ResourceServiceImpl.java
@@ -168,7 +168,25 @@ public class ResourceServiceImpl implements ResourceService {
 
     @Override
     public List<ResourceResponseDTO> getAllResources() {
-        return List.of();
+        List<Resources> resources = resourceRepository.findAll();
+
+        return resources.stream().map(resource->{
+
+            ResourceResponseDTO response = new ResourceResponseDTO();
+            response.setResource_id(resource.getResource_id());
+            response.setResourceCode(resource.getResourceCode());
+            response.setBrand(resource.getBrand());
+            response.setModel(resource.getModel());
+            response.setSpecification(resource.getSpecification());
+            response.setPurchase_date(resource.getPurchaseDate());
+            response.setWarranty_expiry(resource.getWarrantyExpiry());
+            response.setResource_type(resource.getType().getResource_type_name());
+            response.setResource_class(resource.getResourceClass().getResource_class_name());
+            response.setResource_status(resource.getStatus().getResource_status_name());
+            response.setBatchCode(resource.getBatch() != null ? resource.getBatch().getBatchCode() : null);
+
+            return response;
+        }).toList();
     }
 
     @Override

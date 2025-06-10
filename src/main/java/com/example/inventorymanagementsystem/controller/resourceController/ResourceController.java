05ca3333c74fc55c82f1f8394f73be9ef785ebbe
Sugam Shakya <mrsugamshakya@gmail.com>
2025-06-10 15:13:07 +0545

    feat: add ResourceController for ResourceServiceImpl

---
+++ src/main/java/com/example/inventorymanagementsystem/controller/resourceController/ResourceController.java
@@ -0,0 +1,63 @@
+package com.example.inventorymanagementsystem.controller.resourceController;
+
+import com.example.inventorymanagementsystem.dtos.ResourceUpdateDTO;
+import com.example.inventorymanagementsystem.dtos.request.resource.ResourceRequestDTO;
+import com.example.inventorymanagementsystem.dtos.response.resource.ResourceResponseDTO;
+import com.example.inventorymanagementsystem.service.impl.ResourceServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/resources")
+public class ResourceController {
+
+    private final ResourceServiceImpl resourceService;
+
+    public ResourceController(ResourceServiceImpl resourceService){
+        this.resourceService = resourceService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResourceResponseDTO> createResource(@RequestBody ResourceRequestDTO requestDTO){
+        ResourceResponseDTO response = resourceService.createResources(requestDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/batch")
+    public ResponseEntity<List<ResourceResponseDTO>> createResourceBatch(List<ResourceRequestDTO> requestDTOList){
+        List<ResourceResponseDTO> responseDTOList = resourceService.createResourcesInBatch(requestDTOList);
+        return ResponseEntity.ok(responseDTOList);
+    }
+
+    @GetMapping("/{resourceId}")
+    public ResponseEntity<ResourceResponseDTO> getResourceById(@PathVariable("resourceId") Long resourceId){
+        ResourceResponseDTO responseDTO = resourceService.getResourceById(resourceId);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResourceResponseDTO>> getAllResources(){
+        List<ResourceResponseDTO> responseDTOList= resourceService.getAllResources();
+        return ResponseEntity.ok(responseDTOList);
+    }
+
+    @GetMapping("/status/{statusId}")
+    public ResponseEntity<List<ResourceResponseDTO>> getByStatusId(@PathVariable("statusId") Long statusId){
+        List<ResourceResponseDTO> responseDTOList = resourceService.getResourcesByStatus(statusId);
+        return ResponseEntity.ok(responseDTOList);
+    }
+
+    @PutMapping("/{resourceId}")
+    public ResponseEntity<ResourceResponseDTO> updateResource(@PathVariable("resourceId") Long resourceId, ResourceUpdateDTO resourceUpdate){
+        ResourceResponseDTO responseDTO = resourceService.updateResource(resourceId, resourceUpdate);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @DeleteMapping("/{resourceId}")
+    public ResponseEntity<Void> deleteResource(@PathVariable("resourceId") Long resourceId){
+        resourceService.deleteResource(resourceId);
+        return ResponseEntity.noContent().build();
+    }
+}

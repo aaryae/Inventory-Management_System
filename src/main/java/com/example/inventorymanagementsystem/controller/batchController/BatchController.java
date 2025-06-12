463b334cf66ff2b1812f0dc5fe3777da0efff6e4
Sugam Shakya <mrsugamshakya@gmail.com>
2025-06-12 14:49:11 +0545

    feat: add controller for BatchServiceImpl

---
+++ src/main/java/com/example/inventorymanagementsystem/controller/batchController/BatchController.java
@@ -0,0 +1,45 @@
+package com.example.inventorymanagementsystem.controller.batchController;
+
+import com.example.inventorymanagementsystem.dtos.request.BatchRequestDTO;
+import com.example.inventorymanagementsystem.dtos.response.resource.BatchResponseDTO;
+import com.example.inventorymanagementsystem.dtos.response.resource.ResourceResponseDTO;
+import com.example.inventorymanagementsystem.service.impl.BatchServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/batches")
+public class BatchController {
+
+    private final BatchServiceImpl batchService;
+
+    public BatchController(BatchServiceImpl batchService){
+        this.batchService = batchService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BatchResponseDTO> createBatch(@RequestBody BatchRequestDTO requestDTO){
+        BatchResponseDTO responseDTO = batchService.createBatch(requestDTO);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping("/{batchId}")
+    public ResponseEntity<BatchResponseDTO> getBatchById(@PathVariable("batchId") Long batchId){
+        BatchResponseDTO responseDTO = batchService.getBatchById(batchId);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BatchResponseDTO>> getAllBatches(){
+        List<BatchResponseDTO> responseDTOList = batchService.getAllBatches();
+        return ResponseEntity.ok(responseDTOList);
+    }
+
+    @GetMapping("/{batchId}/resources")
+    public ResponseEntity<List<ResourceResponseDTO>> getResourcesByBatch(@PathVariable Long batchId){
+        List<ResourceResponseDTO> responseDTOList = batchService.getResourcesByBatchId(batchId);
+        return ResponseEntity.ok(responseDTOList);
+    }
+}

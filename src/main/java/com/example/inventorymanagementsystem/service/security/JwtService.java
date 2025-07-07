38be96400848f8b174b4dc269e9904c32126c14e
aaryae <aaryaed@gmail.com>
2025-07-07 11:14:28 +0545

    fix: api response changed in dashboard controller

--- src/main/java/com/example/inventorymanagementsystem/service/security/JwtService.java
+++ src/main/java/com/example/inventorymanagementsystem/service/security/JwtService.java
@@ -1,7 +1,6 @@
 package com.example.inventorymanagementsystem.service.security;
 
 import com.example.inventorymanagementsystem.model.User;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface JwtService {
     String generateToken(User user);

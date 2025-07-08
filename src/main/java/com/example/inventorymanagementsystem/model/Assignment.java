393e0ab22d9d6cab48c2ecdf3ae3e8233e14fd8a
Sabinav Joshi <savinabjoshi07@gmail.com>
2025-07-08 13:01:57 +0545

    feat: Updated Employee And Assignment Model

--- src/main/java/com/example/inventorymanagementsystem/model/Assignment.java
+++ src/main/java/com/example/inventorymanagementsystem/model/Assignment.java
@@ -1,28 +1,54 @@
 package com.example.inventorymanagementsystem.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
-import java.time.LocalDate;
-
+import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
+@Table(name = "assignments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Assignment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int assignmentId;
+    @Column(name = "assignment_id")
+    private Long assignmentId;
 
-    int resourceId;
-    int employeeId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "resource_id", nullable = false)
+    private Resource resource;
 
-    LocalDate assignedDate;
-    LocalDate returnDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
 
+    @Column(name = "assigned_date", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime assignedDate;
+
+    @Column(name = "return_date")
+    private LocalDateTime returnDate;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive = true;
+
+    @Column(name = "assigned_by")
+    private String assignedBy; //Username of admin who assigned the resources.
+
+    @Column(name = "return_notes")
+    private String returnNotes;
+
+    //Constructors for new assignment.
+    public Assignment(Resource resource, Employee employee, String assignedBy){
+        this.resource = resource;
+        this.employee = employee;
+        this.assignedBy = assignedBy;
+        this.isActive = true;
+    }
 }
--- src/main/java/com/example/inventorymanagementsystem/model/Employee.java
+++ src/main/java/com/example/inventorymanagementsystem/model/Employee.java
@@ -1,22 +1,51 @@
 package com.example.inventorymanagementsystem.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
-@Getter
-@Setter
+@Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int employeeId;
-    String name;
-    String email;
-    String department;
+    @Column(name = "employee_id")
+    private Long employeeId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String department;
+
+    @OneToMany(mappedBy = "employee",  cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Assignment> assignments;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+
+    //Constructor without ID for Creation
+    public Employee(String name, String email, String department) {
+        this.name = name;
+        this.email = email;
+        this.department = department;
+    }
 }
